0244221a0f7913f0a7ab95ab93fd8ce0f2c007b1
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-09 16:25:08 +0300

    changed the icon and remove unusable buttons

--- script.js
+++ script.js
@@ -1,52 +1,41 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Button from "./button.js"
-const icon = "shopping_cart.svg"
 function App() {
     return (
       <div className="app">
           <div className="item">
             <Button label="Default" title="<Button />"/>
-            <Button label="Default" title="&:hover, &:focus" type="hover" />
           </div>
 
         <div className="item">
-            <Button label="Default" title="<Button variant='outline' />" type="outline" />
-            <Button label="Default" title="&:hover, &:focus" type="outlinehover" />
+            <Button label="Default" title="<Button variant='outline' />" variant="outline" />
         </div>
         <div className="item">
-            <Button label="Default" title="<Button variant='text' />" type="text" />
-            <Button label="Default" title="&:hover, &:focus" type="texthover" />
+            <Button label="Default" title="<Button variant='text' />" variant="text" />
         </div>
         <div className="item">
-            <Button label="Default" title="<Button disableShadow />" type="disable" />
+            <Button label="Default" title="<Button disableShadow />" variant="disabledshadow" />
         </div>
         <div className="item">
-            <Button label="Disable" title="<Button disabled />" type="disableshadow" />
-            <Button label="Disable" title="<Button variant='text' disabled />" type="disabletext" />
+            <Button label="Disable" title="<Button disabled />" variant="disabled" />
+            <Button label="Disable" title="<Button variant='text' disabled />" variant="text" />
         </div>
         <div className="item">
-            <Button label="Default" title="<Button iconStart='local_grocery_store' />" type="iconStart" />
-            <Button label="Default" title="<Button iconEnd='local_grocery_store' />" type="iconEnd" />
+            <Button label="Default" title="<Button iconStart='local_grocery_store' />" variant="iconStart" />
+            <Button label="Default" title="<Button iconEnd='local_grocery_store' />" variant="iconEnd" />
         </div>
         <div className="item">
-            <Button label="Default" title="<Button size='sm' />" type="sm" />
-            <Button label="Default" title="<Button size='md' />" type="md" />
-            <Button label="Default" title="<Button size='lg' />" type="lg" />
+            <Button label="Default" title="<Button size='sm' />" variant="sm" />
+            <Button label="Default" title="<Button size='md' />" variant="md" />
+            <Button label="Default" title="<Button size='lg' />" variant="lg" />
         </div>
         <div className="item">
-            <Button label="Default" title="<Button color='default' />" type="button" />
-            <Button label="Default" title="<Button color='primary' />" type="primary" />
-            <Button label="Secondary" title="<Button color='secondary' />" type="secondary" />
-            <Button label="Danger" title="<Button color='danger' />" type="danger" />
+            <Button label="Default" title="<Button color='default' />" variant="button" />
+            <Button label="Default" title="<Button color='primary' />" variant="primary" />
+            <Button label="Secondary" title="<Button color='secondary' />" variant="secondary" />
+            <Button label="Danger" title="<Button color='danger' />" variant="danger" />
         </div>
-        <div className="item">
-            <Button label="Default" title="&:hover, &:focus" type="hover" />
-            <Button label="Default" title="&:hover, &:focus" type="primaryhover" />
-            <Button label="Secondary" title="&:hover, &:focus" type="secondaryhover" />
-            <Button label="Danger" title="&:hover, &:focus" type="dangerhover" />
-        </div>
-
 
       </div>
     );
