fb97ad153efa2752fff3d25cfdec1020961ff475
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-12 07:07:21 +0300

    tested the button.js

--- button.js
+++ button.js
@@ -1,16 +1,17 @@
-import React from "react"
+ import React from "react"
 
 
-function Button(props) {
-    const className = `button ${props.variant}`
+  function Button(props) {
+      const className = `button ${props.variant}`
    
-    return (
-    <div className="buttontype">
-      <p>{props.title}</p>
-      <button className={className}>
-        {props.icon && props.icon} {props.label} {props.disabled}
-      </button>
-    </div>
-    );
-   }
-export default Button;
+      return (
+      <div className="buttontype">
+        <p>{props.title}</p>
+        <button className={className}>
+          {props.icon && props.icon} {props.label} {props.disabled}
+        </button>
+      </div>
+     );
+    }
+ export default Button;
+
